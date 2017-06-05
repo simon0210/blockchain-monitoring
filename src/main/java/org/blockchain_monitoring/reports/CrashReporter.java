91c83f48f47400f230e09877dca5d17175691778
Ruslan Kryukov <ruslan.kryukov@ru.ibm.com>
2017-06-05 19:18:41 +0300

    Crash Reporter: add zipped logs to report

--- src/main/java/org/blockchain_monitoring/reports/CrashReporter.java
+++ src/main/java/org/blockchain_monitoring/reports/CrashReporter.java
@@ -3,15 +3,22 @@ package org.blockchain_monitoring.reports;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
+import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.net.URI;
 import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by Ruslan Kryukov on 01/06/2017.
@@ -20,7 +27,7 @@ public class CrashReporter {
     private static String reportsURL;
     private static String userEmail;
     private static String userIP;
-    private static boolean isReportsEnables;
+    private static boolean isReportsEnabled;
     private static ObjectMapper mapper = new ObjectMapper(new JsonFactory());
     private static HttpClient httpClient = HttpClientBuilder.create().build();
 
@@ -30,7 +37,7 @@ public class CrashReporter {
             app.run();
 
         } catch (Exception ex) {
-            if(isReportsEnables) {
+            if(isReportsEnabled) {
                 ReportInfo reportInfo = new ReportInfo(ex, userEmail, userIP);
                 try {
                     String json = mapper.writeValueAsString(reportInfo);
@@ -49,8 +56,8 @@ public class CrashReporter {
             reportsURL = properties.getProperty("reports.url");
             userEmail = System.getenv("USER_EMAIL");
             userEmail = userEmail != null ? userEmail : "not_provided@email.null";
-            String reportsEnabled = System.getenv("REPORTS_ENABLED");
-            isReportsEnables = reportsEnabled != null && reportsEnabled.toLowerCase().equals("true");
+            String reportsDisabled = System.getenv("REPORTS_DISABLED");
+            isReportsEnabled = reportsDisabled == null || reportsDisabled.toLowerCase().equals("false");
             try {
                 userIP = IOUtils.toString(new URI("https://myexternalip.com/raw")).replace("\n", "");
             } catch (Exception ignore) {
@@ -63,15 +70,46 @@ public class CrashReporter {
 
     private static void sendReport(String report) throws Exception {
         HttpPost request = new HttpPost(reportsURL);
-        StringEntity params = new StringEntity(report);
-        request.addHeader("content-type", "application/x-www-form-urlencoded");
-        request.addHeader("X-BLOCKCHAIN-MONITORING-APP", "Crash-Report");
-        request.setEntity(params);
+        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
 
+        builder.addTextBody("report", report, ContentType.APPLICATION_JSON);
+
+        File packedLog = packLogs();
+
+        if(packedLog != null) {
+            builder.addBinaryBody("logfile", packedLog, ContentType.APPLICATION_OCTET_STREAM,
+                    "monitoring.zip");
+        }
+
+        HttpEntity multipart = builder.build();
+        request.setEntity(multipart);
         HttpResponse response = httpClient.execute(request);
 
         if(response.getStatusLine().getStatusCode() != 200) {
             throw new RuntimeException("Failure when tried to send report!");
+        }
+    }
+
+    private static File packLogs() {
+        try {
+            File packedZipArchive = new File("/var/logs/fabric-monitoring/monitoring.log.zip");
+            ZipOutputStream out = new ZipOutputStream(new FileOutputStream(packedZipArchive));
+            out.setLevel(9);
+            ZipEntry zipEntry = new ZipEntry("monitoring.log");
+            out.putNextEntry(zipEntry);
+
+            byte[] data = IOUtils.toString(
+                    new FileInputStream(new File("/var/logs/fabric-monitoring/monitoring.log"))).getBytes();
+
+            out.write(data, 0, data.length);
+            out.closeEntry();
+            out.close();
+
+            return packedZipArchive;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
         }
     }
 }
