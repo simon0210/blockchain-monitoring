6c6f68be1e46306a03d0067792cdac653d6a8f6c
andreevym <Yury.Andreev@ru.ibm.com>
2017-07-22 13:33:52 +0300

    delete old data after :TIME_EVENT_LIFETIME

--- src/main/java/org/blockchain_monitoring/scheduler/ScheduledTasks.java
+++ src/main/java/org/blockchain_monitoring/scheduler/ScheduledTasks.java
@@ -1,14 +1,22 @@
 package org.blockchain_monitoring.scheduler;
 
+import java.text.SimpleDateFormat;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.Date;
+
+import javax.annotation.PostConstruct;
+
+import org.blockchain_monitoring.api.InfluxDestroyer;
+import org.blockchain_monitoring.api.InfluxDestroyerException;
 import org.blockchain_monitoring.fly_client_spring.FlyNet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Component
 public class ScheduledTasks {
@@ -23,10 +31,38 @@ public class ScheduledTasks {
     @Autowired
     public FlyNet flyNet;
 
+    @Value("${TIME_EVENT_LIFETIME:00:01:00}")
+    private String envTimeEventLifetime;
+
+    @Autowired
+    private InfluxDestroyer influxDestroyer;
+
+    private LocalTime timeEventLifetime;
+
+    @PostConstruct
+    public void init() {
+        // be careful that @Value init only after @PostConstruct not in Constructor
+        timeEventLifetime = LocalTime.parse(envTimeEventLifetime);
+    }
+
     @Scheduled(fixedDelayString="${SCHEDULED_TASKS_DELAY:1000}")
     public void reportCurrentTime() {
         log.debug("START The time is now {}", dateFormat.format(new Date()));
         flyNet.getOrganisations().forEach(metricWriter);
+
+        clearOldData("query");
+        clearOldData("invoke");
+        clearOldData("commonBlockEvent");
+
         log.debug("END The time is now {}", dateFormat.format(new Date()));
+    }
+
+    private void clearOldData(String measurement) {
+        try {
+            final LocalDateTime localDateTime = LocalDateTime.now(Clock.systemUTC()).minusSeconds(timeEventLifetime.toSecondOfDay());
+            influxDestroyer.deleteMeasurementOlderTime(measurement, localDateTime);
+        } catch (InfluxDestroyerException e) {
+            log.error(e.getMessage(), e);
+        }
     }
 }
